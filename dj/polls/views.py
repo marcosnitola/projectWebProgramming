0381c44d5168f27fac76430f7257398f0376c94f
marcosraccoon <marcos123.mdnr@gmail.com>
2022-09-06 17:11:57 -0500

    Views modified

--- dj/polls/views.py
+++ dj/polls/views.py
@@ -2,6 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.template import loader
+
+
 
 def index(request):
-    return HttpResponse('Hello world')
+    #Archivo HTML con template
+    template = loader.get_template('index.html')
+    #logica de la vista
+    context = {}
+    #respuesta
+    return HttpResponse(template.render(context,request))

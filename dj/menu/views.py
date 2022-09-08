af8c2c0ff67ab256db47da76b67ad05eca03921e
marcosraccoon <marcos123.mdnr@gmail.com>
2022-09-08 06:54:50 -0500

    Setting menu views and urls

--- dj/menu/views.py
+++ dj/menu/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+from django.template import loader
+
+def menu (request):
+    #Archivo HTML con template
+    template = loader.get_template('menu.html')
+    #logica de la vista
+    context = {}
+    #respuesta
+    return HttpResponse(template.render(context,request))

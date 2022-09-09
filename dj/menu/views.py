1978e71ccd88d013646be669e6891f725d411389
marcosraccoon <marcos123.mdnr@gmail.com>
2022-09-09 15:30:29 -0500

    Allow access to the Platillo and Categoria models from template

--- dj/menu/views.py
+++ dj/menu/views.py
@@ -4,10 +4,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
+from .models import Platillo, Categoria
 def menu (request):
+    menu_list = Platillo.objects.all()
+    cat_list = Categoria.objects.all()
     #Archivo HTML con template
     template = loader.get_template('menu.html')
     #logica de la vista
-    context = {}
+    context = {
+            "menu_list": menu_list,
+            "cat_list": cat_list
+    }
     #respuesta
     return HttpResponse(template.render(context,request))

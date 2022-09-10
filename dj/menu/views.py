817be1d6c234731d99fb0f9afe314856aa0a8500
marcosraccoon <marcos123.mdnr@gmail.com>
2022-09-10 16:23:39 -0500

    Menu page updated and divided into drinks and food

--- dj/menu/views.py
+++ dj/menu/views.py
@@ -7,12 +7,17 @@ from django.template import loader
 from .models import Platillo, Categoria
 def menu (request):
     menu_list = Platillo.objects.all()
+    comida_list = Platillo.objects.filter(tipo=2)
+    bebida_list = Platillo.objects.filter(tipo=1)
     cat_list = Categoria.objects.all()
+
     #Archivo HTML con template
     template = loader.get_template('menu.html')
     #logica de la vista
     context = {
             "menu_list": menu_list,
+            "comida_list": comida_list,
+            "bebida_list": bebida_list,
             "cat_list": cat_list
     }
     #respuesta

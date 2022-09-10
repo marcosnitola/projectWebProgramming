65b4763569737befb7011dbd4502a2ce38986a7b
marcosraccoon <marcos123.mdnr@gmail.com>
2022-09-10 16:14:27 -0500

    Popularidad field added and __str__ method for models defined

--- dj/menu/models.py
+++ dj/menu/models.py
@@ -3,10 +3,15 @@ from django.db import models
 # Create your models here.
 class Categoria(models.Model):
     nombre = models.CharField(max_length=64)
+    def __str__(self):
+        return self.nombre
 
 class Platillo(models.Model):
     nombre = models.CharField(max_length=64)
     precio = models.PositiveIntegerField()
+    popularidad = models.PositiveSmallIntegerField(default=0)
     tipo = models.ForeignKey(Categoria, on_delete=models.CASCADE)
     descripcion = models.CharField(max_length=128)
     img_url = models.CharField(max_length=256)
+    def __str__(self):
+        return self.nombre

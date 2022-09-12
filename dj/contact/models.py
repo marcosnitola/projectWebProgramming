d32d523a2e8f1e5321fdbb47ddd1c9219eae8cf0
marcosraccoon <marcos123.mdnr@gmail.com>
2022-09-12 10:04:26 -0500

    Model for contact message

--- dj/contact/models.py
+++ dj/contact/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class ContactMessage(models.Model):
+    nombre_cliente = models.CharField(max_length=128)
+    correo_cliente = models.EmailField(max_length=512)
+    telefono_cliente = models.PositiveBigIntegerField()
+    mensaje_cliente = models.CharField(max_length=1024)
+    def __str__(self):
+        return self.correo_cliente

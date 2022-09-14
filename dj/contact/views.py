fbaafbea9fc48aeb935d5d424472a5b022793081
marcosraccoon <marcos123.mdnr@gmail.com>
2022-09-14 17:40:15 -0500

    Form functionality added

--- dj/contact/views.py
+++ dj/contact/views.py
@@ -9,12 +9,20 @@ from .models import ContactMessage
 from .forms import ContactForm
 def contact (request):
     template = loader.get_template('contact.html')
-    form = ContactForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-        return redirect('')
+    if request.POST:
+        newmessage = ContactMessage(
+                nombre_cliente = request.POST["nombres"],
+                correo_cliente = request.POST["correo"],
+                telefono_cliente = request.POST["celular"],
+                mensaje_cliente = request.POST["mensaje"],
+        )
+        newmessage.save()
+    #form = ContactForm(request.POST or None)
+    #if form.is_valid():
+    #    form.save()
+    #    return redirect('')
     context = {
-        "form": form
+        #"form": form
     }
-    return HttpResponse(template.render(context, request))
-    # return render(request,'contact.html', context)
+    #return HttpResponse(template.render(context, request))
+    return render(request,'contact.html', context)
